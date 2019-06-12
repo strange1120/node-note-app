e9cb3bba0139bedab22ba39f18164918877e0254
Sarah Strange <strange1120@gmail.com>
2019-06-12 01:09:37 -0400

    adding node modules

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
+const chalk = require('chalk')
+const chalkAnimation = require('chalk-animation');
 const getNotes = require("./notes.js");
 
 const text = getNotes();
 console.log(text);
+
+console.log(chalk.bgGreen('Success!'))
+const rainbow = chalkAnimation.rainbow('Lorem ipsum'); // Animation starts
+
+setTimeout(() => {
+    rainbow.stop(); // Animation stops
+}, 1000);
+
+setTimeout(() => {
+    rainbow.start(); // Animation resumes
+}, 2000);
