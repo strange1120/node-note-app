92697ae1ca6558f85c8e4f188a20abf011f03de9
Sarah Strange <sarah.strange@availity.com>
2019-07-10 11:37:19 -0400

    adding a note, reading and writing to file sync, preventing duplicates

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const chalk = require("chalk");
 const chalkAnimation = require("chalk-animation");
-const getNotes = require("./notes.js");
+const notes = require("./notes.js");
 const yargs = require("yargs");
 
 yargs.version("1.1.0");
@@ -21,8 +21,7 @@ yargs.command({
       }
   },
   handler: (argv) => {
-    console.log('Title: ' + argv.title)
-    console.log('Body: ' + argv.body)
+    notes.addNote(argv.title, argv.body)
   }
 })
 
