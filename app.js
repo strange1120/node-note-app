7e7ce1e5eb171a7194bcb820b08e89ed1a8cb90c
Sarah Strange <sarah.strange@availity.com>
2019-07-18 23:12:08 -0400

    removing a note

--- app.js
+++ app.js
@@ -28,8 +28,15 @@ yargs.command({
 yargs.command({
   command: 'remove',
   describe: 'Removing a note',
-  handler: () => {
-    console.log('Removing a note')
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: (argv) => {
+    notes.removeNote(argv.title)
   }
 })
 
--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const chalk = require("chalk");
 
 const getNotes = () => {
   return "Your notes..";
@@ -18,12 +19,28 @@ const addNote = (title, body) => {
   });
 
   saveNotes(notes);
-  console.log("New note added")
+  console.log(chalk.green.inverse('Note added!'))
   } else {
-    console.log("Note title taken")
+    console.log(chalk.red.inverse("Note title taken"))
   }
  
 };
+
+const removeNote = title => {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter((note)=> {
+    return note.title !== title
+  })
+
+  if(notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse('Note removed'))
+  saveNotes(notesToKeep)
+
+  } else {
+    console.log(chalk.red.inverse('No note found!'))
+  }
+
+}
 
 const saveNotes = notes => {
   const dataJSON = JSON.stringify(notes);
@@ -42,5 +59,7 @@ const loadNotes = () => {
 
 module.exports = {
   getNotes: getNotes,
-  addNote: addNote
+  addNote: addNote,
+  removeNote: removeNote
+
 };
